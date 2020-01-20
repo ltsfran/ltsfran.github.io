bd699b7b6c6d2f456d681972109617720c96fb2e
Luis Tupa <luis.tupa@orbis.com.pe>
2020-01-20 18:13:55 -0500

    feat(app): add integration with dreamtec-ui

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'dreamtec-ui';
+
+export const App = () => (
+  <>
+    <div>How are you world?</div>
+    <Button>Get Started</Button>
+  </>
+);
 
 ReactDOM.render(
-  <div>How are you world?</div>,
+  <App />,
   document.getElementById('root')
 );
--- app/index.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-const rootPath = path.resolve(__dirname, '../../client');
-// const test = path.
-
-console.log(rootPath, 'rootPath');
