ebff3a877446c12e865573903ed6cad18f13bc6a
ltsfran <luistupa6@gmail.com>
2021-11-21 23:16:54 +0000

    Deploying to gh-pages from  @ 6f99ea57506ff000b8e1f236a147a7a74d73c576 🚀

---
+++ _next/static/_ESkp9wKho1mE20buBX46/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ae12d7c8ff9637bf.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
