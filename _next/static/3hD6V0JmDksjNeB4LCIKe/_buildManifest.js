ce496af13c67c52204d9c24623944bebc50998bf
ltsfran <>
2021-12-05 20:22:14 +0000

    Deploying to gh-pages from @ ltsfran/ltsfran.github.io@cf9238d4ccb0e08c549e9dd44045048514c1ab92 🚀

---
+++ _next/static/3hD6V0JmDksjNeB4LCIKe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-64d71849570f15f0.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
